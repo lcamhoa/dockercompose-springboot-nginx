577dd231fc56796f83c001391a872ce1f3b4a0c9
root <root@localhost.localdomain>
2020-11-27 06:10:39 +0700

    Log Nginx passed headers

--- app/src/main/java/com/hellokoding/springboot/IndexController.java
+++ app/src/main/java/com/hellokoding/springboot/IndexController.java
@@ -1,14 +1,26 @@
 package com.hellokoding.springboot;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class IndexController {
+	
     @GetMapping("/")
-    public String index() {
+    public String index(@RequestHeader Map<String,String> httpHeader,
+    		@ModelAttribute("model") ModelMap model) {
+    	List<String> headers = httpHeader.entrySet()
+    			.stream()
+    			.map(e -> e.getKey() + "," + e.getValue())
+    			.collect(Collectors.toList());
+    	model.addAttribute("words", headers);
         return "index";
     }
 }
--- app/src/main/resources/templates/index.ftl
+++ app/src/main/resources/templates/index.ftl
@@ -6,5 +6,12 @@
 </head>
 <body>
     <h1>Hello Docker and Spring Boot!</h1>
+    <h2>Http Headers</h2>
+    <ul>
+    
+	<#list model["words"] as word>
+	    <li>${word}</li>
+	</#list>
+	</ul>
 </body>
 </html>
